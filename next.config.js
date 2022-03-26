af3c9655f16415fb4285b0f23778ac921ea6645f
MurariSabavath <smurari1004@gmail.com>
2022-03-27 00:44:47 +0530

    add styled components compiler

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ module.exports = {
     webpack5: true, // by default, if you customize webpack config, they switch back to version 4.
     // Looks like backward compatibility approach.
   },
+  compiler: {
+    styledComponents: true,
+  },
   webpack(config) {
     config.resolve.fallback = {
       ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
